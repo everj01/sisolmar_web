320da9c510fbed3edcd27194c513390354b34a21
Rodrigo <rodrigolauramoreno@gmail.com>
2026-05-26 11:03:15 -0500

    style: eliminar bloques PHPDoc redundantes en ProcesarCursosPeriodicosVencidos
    - Remueve comentarios PHPDoc de firma y descripción del comando
      que duplicaban la información ya declarada en las propiedades
       y  de la clase Command.

--- app/Console/Commands/ProcesarCursosPeriodicosVencidos.php
+++ app/Console/Commands/ProcesarCursosPeriodicosVencidos.php
@@ -11,18 +11,8 @@ use Illuminate\Support\Facades\Log;
 
 class ProcesarCursosPeriodicosVencidos extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'capacitacion:procesar-cursos-periodicos';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Procesa cursos periódicos vencidos, cerrando programaciones vigentes y generando nuevas programaciones pendientes.';
 
     public function handle()
