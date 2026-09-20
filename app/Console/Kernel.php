12420d8048170add92114833e3dc5f9e6c7fb2b6
Rodrigo <dr3amerz0mb1e97@gmail.com>
2026-09-19 19:34:25 -0500

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,19 +24,19 @@ class Kernel extends ConsoleKernel
                      \Log::error('Error al ejecutar el comando de envío de alertas de caducidad.');
                  });
 
-         $schedule->command('capacitacion:enviar-recordatorios-curso')
-                   ->weeklyOn(1, '08:45')
-                   ->withoutOverlapping()
-                   ->runInBackground()
-                   ->onSuccess(function () {
-                       \Log::info('Comando de recordatorios de curso ejecutado exitosamente.');
-                   })
-                   ->onFailure(function () {
-                       \Log::error('Error al ejecutar el comando de recordatorios de curso.');
-                   });
+        //  $schedule->command('capacitacion:enviar-recordatorios-curso')
+        //            ->weeklyOn(1, '08:45')
+        //            ->withoutOverlapping()
+        //            ->runInBackground()
+        //            ->onSuccess(function () {
+        //                \Log::info('Comando de recordatorios de curso ejecutado exitosamente.');
+        //            })
+        //            ->onFailure(function () {
+        //                \Log::error('Error al ejecutar el comando de recordatorios de curso.');
+        //            });
 
           $schedule->command('capacitacion:procesar-cursos')
-                   ->dailyAt('09:30')
+                   ->dailyAt('12:30')
                    ->withoutOverlapping()
                    ->onSuccess(function () {
                        \Log::info('Comando de procesamiento de cursos periódicos ejecutado exitosamente.');
